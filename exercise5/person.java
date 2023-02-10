08e680f9201bf2a8022c3994f3a7083300c5cc9d
Nivek Reyes <119916376+DahMo@users.noreply.github.com>
2023-02-10 15:29:08 +0800

    Update person.java

---
+++ exercise5/person.java
@@ -0,0 +1,14 @@
+public class person{
+
+    private String name;
+
+    public String getName(){
+        return this.name;
+    
+    }
+
+    public void setname(String newname){
+
+        this.name = newname;
+    }
+}
